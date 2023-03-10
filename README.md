3f357e18ab38966a3eb277c99a8fbb2332dc745b
Iaroslav Pyrogov <go.go.jar@gmail.com>
2023-03-10 16:36:41 +0700

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,139 @@
-# music_app_controller
-Music app controller for Mac OS
+Introduction
+------------
+
+The Music App Controller for Mac OS is a Python script that allows users to control their music app from the command line. This project is open-source and aims to provide a simple way to schedule and play music based on a list of activities.
+
+Features
+--------
+
+* Pause and resume the music app
+* Play music for specific activities
+* Schedule activities for specific times of the day
+* Notifications for when music starts and stops
+
+Installation
+------------
+
+1. Install Python 3 if it's not already installed on your system.
+2. Clone the repository from GitHub: `git clone https://github.com/yarka-guru/music_app_controller.git`
+3. Install the required dependencies: `pip install -r requirements.txt`
+4. Set up environment variables by creating a `.env` file and adding the following variables:
+
+makefile
+
+```makefile
+MUSIC_DIR=/path/to/music/directory
+SCHEDULE_DIR=/path/to/schedule/directory
+LOG_FILE=/path/to/log/file
+MUSIC_APP_NAME=name_of_music_app
+VOLUME=volume_level
+ACTIVITY_FILES={"0": "monday.txt", "1": "tuesday.txt", "2": "wednesday.txt", "3": "thursday.txt", "4": "friday.txt", "5": "saturday.txt", "6": "sunday.txt"}
+LANGUAGES=en,fr,es,de
+```
+
+Usage
+-----
+
+### Pause and resume music
+
+To pause the music, run the following command:
+
+css
+
+```css
+python music_app_controller.py pause
+```
+
+To resume the music, run the following command:
+
+lua
+
+```lua
+python music_app_controller.py resume
+```
+
+### Play music for an activity
+
+To play music for a specific activity, run the following command:
+
+`python music_app_controller.py play activity_name`
+
+Replace `activity_name` with the name of the activity you want to play music for. For example:
+
+`python music_app_controller.py play exercise`
+
+### Schedule activities
+
+To schedule activities, create a text file for each day of the week in the `SCHEDULE_DIR` directory. The name of the file should be the name of the day (e.g., `monday.txt`, `tuesday.txt`, etc.).
+
+In each file, add a list of activities in the following format:
+
+makefile
+
+```makefile
+HH:MM - activity_name
+```
+
+Replace `HH:MM` with the time you want the activity to start (in 24-hour format), and `activity_name` with the name of the activity.
+
+For example:
+
+makefile
+
+```makefile
+09:00 - exercise
+12:00 - lunch
+15:00 - tea_break
+18:00 - dinner
+20:00 - relax
+22:00 - sleep
+```
+
+### Run the music scheduler
+
+To run the music scheduler, run the following command:
+
+`python music_app_controller.py`
+
+This will start the scheduler, and music will be played automatically for the scheduled activities.
+
+# Setting up Autoloader
+
+In order to run the script automatically every time the system starts, you can set up a Launch Agent.
+
+1. Create a new property list file named startup.plist in the ~/Library/LaunchAgents directory using the command nano ~/Library/LaunchAgents/startup.plist
+2. Copy the following code and paste it into the startup.plist file:
+
+``` xml
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+<plist version="1.0">
+<dict>
+    <key>Label</key>
+    <string>com.startup</string>
+    <key>ProgramArguments</key>
+    <array>
+        <string>/usr/bin/python3</string>
+        <string>/path/to/your/script.py</string>
+    </array>
+    <key>RunAtLoad</key>
+    <true/>
+</dict>
+</plist>
+```
+
+Contributing
+------------
+
+We welcome contributions from the community. To contribute to the project, please follow these steps:
+
+1. Fork the repository.
+2. Create a new branch for your feature or bug fix: `git checkout -b feature/your_feature_name`
+3. Make your changes and commit them: `git commit -m "Your commit message"`
+4. Push your changes to your fork: `git push origin feature/your_feature_name`
+5. Submit a pull request to the main repository.
+
+Conclusion
+----------
+
+We hope this documentation helps you get started with the Music App Controller for Mac
---
+++ music_app_controller.py
@@ -0,0 +1,175 @@
+'''Music app controller for Mac OS'''
+import datetime
+import logging
+import os
+import subprocess
+import time
+import json
+import pync
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MUSIC_DIR = os.getenv("MUSIC_DIR")
+SCHEDULE_DIR = os.getenv("SCHEDULE_DIR")
+LOG_FILE = os.getenv("LOG_FILE")
+MUSIC_APP_NAME = os.getenv("MUSIC_APP_NAME")
+VOLUME = os.getenv("VOLUME")
+ACTIVITY_FILES = json.loads(os.getenv("ACTIVITY_FILES"))
+LANGUAGES = os.environ.get("LANGUAGES", "").split(",")
+
+class MusicAppController:
+    """Class for controlling music app on Mac OS"""
+    def __init__(self, music_app_name=MUSIC_APP_NAME):
+        self.music_app_name = music_app_name
+
+    def pause(self):
+        """Pause music app if it is running"""
+        if self._is_running():
+            subprocess.run(
+                ["osascript", "-e", f'tell application "{self.music_app_name}" to pause'],
+                check=True,
+            )
+
+    def resume(self):
+        """Resume music app if it is running, otherwise open it"""
+        if self._is_running():
+            subprocess.run(
+                ["osascript", "-e", f'tell application "{self.music_app_name}" to play'],
+                check=True,
+            )
+        else:
+            subprocess.run(["open", "-a", self.music_app_name], check=True)
+
+    def play(self, activity):
+        """Play music for activity"""
+        if activity == "stop_music":
+            self.pause()
+            return
+
+        for language in LANGUAGES:
+            music_file = os.path.join(MUSIC_DIR, language, f"{activity}.wav")
+            try:
+                self.pause()
+                subprocess.run(["afplay", music_file], check=True)
+            except FileNotFoundError:
+                logging.error("Music file not found: %s", music_file)
+            if activity != "stop_music":
+                self.resume()
+
+
+    def _is_running(self):
+        """Check if the music app is running"""
+        check_app = subprocess.run(
+            ["pgrep", "-x", self.music_app_name],
+            stdout=subprocess.PIPE,
+            encoding="utf-8",
+            check=False,
+        )
+        return check_app.returncode == 0
+
+
+class MusicScheduler:
+    """Class for scheduling music"""
+    def __init__(self, music_app_controller, schedule_dir=SCHEDULE_DIR):
+        self.music_app_controller = music_app_controller
+        self.schedule_dir = schedule_dir
+        self.activities = self._read_activities()
+        self.current_activity = ""
+
+    def run(self):
+        """Run the music scheduler"""
+        self._setup_logging()
+        self._setup_notifications()
+        self._run()
+
+    def _setup_logging(self):
+        logging.basicConfig(
+            filename=LOG_FILE,
+            level=logging.INFO,
+            format="%(asctime)s %(levelname)s %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+        logging.info("Music app controller started")
+
+    def _setup_notifications(self):
+        pync.notify("Music app controller started", title="Music App")
+
+    def _run(self):
+        while True:
+            now = datetime.datetime.now()
+            current_hour, current_minute = now.hour, now.minute
+
+            for activity_time, activity in self.activities:
+                if (
+                    activity_time.hour == current_hour
+                    and activity_time.minute == current_minute
+                ):
+                    if self.current_activity == "stop_music":
+                        self.music_app_controller.pause()
+                        logging.info(
+                            "Music stopped at %s", activity_time.strftime("%H:%M")
+                        )
+                        pync.notify("Music stopped", title="Music App")
+                    elif self.current_activity != activity:
+                        self.current_activity = activity
+                        logging.info(
+                            "Playing music for activity %s at %s",
+                            activity,
+                            activity_time.strftime("%H:%M"),
+                        )
+                        pync.notify(activity, title="Music App")
+                        self.music_app_controller.play(activity)
+
+            # Sleep for 30 seconds
+            time.sleep(30)
+
+    def _read_activities(self):
+        filename = self._get_filename()
+        try:
+            with open(filename, "r", encoding="utf-8") as file:
+                lines = file.readlines()
+        except FileNotFoundError:
+            logging.error("Activity list file not found: %s", filename)
+            return []
+
+        activities = []
+        for line in lines:
+            try:
+                time_str, activity = line.split(" - ", 1)
+                activity_time = datetime.datetime.strptime(time_str, "%H:%M")
+                activities.append((activity_time, activity.strip()))
+            except ValueError:
+                logging.error(
+                    "Incorrect format in activity list file: %s", line.strip()
+                )
+
+        return activities
+
+    def _get_filename(self):
+        activity_files_json = os.getenv("ACTIVITY_FILES")
+        activity_files_dict = json.loads(activity_files_json)
+        today = datetime.datetime.today()
+        filename = activity_files_dict.get(str(today.weekday()))
+        return os.path.join(self.schedule_dir, filename)
+
+    def get_current_activity(self):
+        '''Get current activity'''
+        return self.current_activity
+
+    def get_activities(self):
+        '''Get activities'''
+        return self.activities
+
+    def set_activities(self, activities):
+        '''Set activities'''
+        self.activities = activities
+
+def main():
+    """Main function"""
+    music_app_controller = MusicAppController()
+    music_scheduler = MusicScheduler(music_app_controller)
+    music_scheduler.run()
+
+if __name__ == "__main__":
+    main()
